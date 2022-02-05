9059b3a27bc2e264b5ab9e16d60a4421c2d4f690
techmaxus <lakshay.mc19@nsut.ac.in>
2022-02-05 16:50:21 +0530

    added files

---
+++ LeetCode_Daily/feb-22/05-02-2022.cpp
@@ -0,0 +1,35 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        if(lists.size()==0) return nullptr;
+        while(lists.size() > 1) {
+            lists.push_back(merge(lists[0], lists[1]));
+            lists.erase(lists.begin());
+            lists.erase(lists.begin());
+        }
+        return lists[0];
+    }
+    
+    ListNode* merge(ListNode* l1, ListNode* l2) {
+        if(!l1) return l2;
+        if(!l2) return l1;
+        
+        if(l1->val < l2->val){
+            l1->next = merge(l1->next, l2);
+            return l1;
+        } else {
+            l2->next = merge(l2->next, l1);
+            return l2;
+        }
+    }
+};
