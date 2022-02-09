7fb4b2b1bef063282cc06f6dd78fdc623cc1a3c5
techmaxus <lakshay.mc19@nsut.ac.in>
2022-02-09 22:16:43 +0530

    heap theory

--- dsa_essentials/Priority_Queue/heap.h
+++ dsa_essentials/Priority_Queue/heap.h
@@ -3,10 +3,51 @@ using namespace std;
 
 class Heap{
     vector<int>v;
+    void heapify(int i){
+
+        int left=2*i;
+        int right =2*i+1;
+
+        int minIdx =i;
+        if(left<v.size() and v[left]<v[i]){
+            minIdx = left;
+        }
+        if(right<v.size() and v[right]<v[minIdx]){
+            minIdx = right;
+        }
+
+        if(minIdx!=i){
+            swap(v[i],v[minIdx]);
+            heapify(minIdx); 
+        }
+    }
 
 public:
-    Heap(int Defaut_size=10){
-        
+    Heap(int default_size=10){
+        v.reserve(default_size+1);
+        v.push_back(-1);
+    }
+    void push (int data){
+
+        v.push_back(data);
+
+        int idx= v.size()-1;
+        int parent =idx/2;
+
+        while(idx>1 and v[idx]<v[parent] ){
+            swap(v[idx], v[parent]);
+            idx=parent;
+            parent=parent/2;
+
+        }
+    }
+    int top(){
+        return v[1];        
+    }
+    void pop(){
+        swap(v[1],v[v.size()-1]);
+        v.pop_back();
+        heapify(1);       
     }
 
 };
